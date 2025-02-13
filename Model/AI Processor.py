34570d7cff0bc9093918b35410a0c2d8d02c7790
nb101bn <nathanbeach123@gmail.com>
2025-02-13 11:49:26 -0600

    Start
    This is the start of the CNN part of the neural networks going to have multiple different neural networks for different uses. Will most likely have to change the names of some of the CNNs to be more specific with their task. This one will be handeling surface analysis.

--- Model/AI Processor.py
+++ Model/AI Processor.py
@@ -4,3 +4,17 @@ import torchaudio
 import requests
 import pandas as pd
 from datetime import datetime
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision import datasets
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv_layer_1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv_layer_2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.relu_layer = nn.ReLU()
+        self.max_pool_layer = nn.MaxPool2d(kernel_size=2, stirde=1)
+        
